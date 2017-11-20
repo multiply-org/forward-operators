12778bafb37547744429e09ad5e8efa8f3a27ac4
Jose Gomez-Dans <jgomezdans@gmail.com>
2017-11-20 15:52:44 +0000

    Added the optical forward model. Uses the prosail bindings, has some tests

--- multiply_forward_operators/optical_forward_model.py
+++ multiply_forward_operators/optical_forward_model.py
@@ -18,7 +18,24 @@ __email__ = "j.gomez-dans@ucl.ac.uk"
 def optical_forward_operator(x, sza, vza, raa, version="PROSAIL_D",
                              hspot=0.01):
     """A generic wrapper to the PROSPECT+SAIL operators. Uses either PROSPECT D
-    or PROSPECT 5"""
+    or PROSPECT 5. The state vector is given as a 1D vector, with parameters
+    in order:
+    1. N (leaf layers)
+    2. Ant (if using PROSPECT-D, Anthocyanins, ug/cm2)
+    3. Cab (leaf chlorophyll conc, ug/cm2)
+    4. Car (leaf carotenoids conc, ug/cm2)
+    5. Cbrown (senescent pigment-like concentration, -)
+    6. Cw (equivalent leaf water thickness, cm)
+    7. Cm (dry matter, g/cm2)
+    8. LAI (leaf area index, m2/m2)
+    9. ALA (average leaf angle, deg)
+    10. rsoil (dry soil scalar)
+    11. psoil (wet soil scalar)
+
+    Additionally, one needs to pass the different view/illumination angles, and
+    optionally the value of the hotspot parameter. The function returns the
+    top of canopy reflectance between 400 and 2500 nm every 1 nm.
+    """
     if not version.upper() in ["PROSAIL_D", "PROSAIL_5"]:
         raise ValueError("Can only deal with SAIL + PROSPECT D or 5!")
 
