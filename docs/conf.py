19fbd94b86d0f80114f9570ee8fb8de9aaee728c
McWhity <weiss.thomas@lmu.de>
2018-11-16 19:28:57 +0100

    debug cod

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ if on_rtd:
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.coverage',
               'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
-              'sphinx.ext.napoleon', 'sphinxcontrib.bibtex']
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
