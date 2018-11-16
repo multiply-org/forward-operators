bda90f4dc466589bc7e7c417d5e8285418644ae7
McWhity <weiss.thomas@lmu.de>
2018-11-16 19:40:55 +0100

    debug doc

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,8 @@ import os
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "multiply_forward_operator")))
 
 # -- Hack for ReadTheDocs ------------------------------------------------------
 # This hack is necessary since RTD does not issue `sphinx-apidoc` before running
