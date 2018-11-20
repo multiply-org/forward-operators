11b1e802fa91efeb53b173e46f157f9038c615a3
McWhity <weiss.thomas@lmu.de>
2018-11-20 16:18:50 +0100

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ if on_rtd:
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.coverage',
               'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',  'sphinxcontrib.bibtex']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
